bb1a1e6a68b18952d01772f11eaeff6fc2f901f8
Serena <115725865+serenaneg@users.noreply.github.com>
2022-11-25 17:31:28 +0100

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -59,4 +59,41 @@ f2006 <- 178075/(178075 + 164651) #0.52 51% forest
 h2006 <- 164651/(178075 + 164651) #0.48 48% human
 
 #create a table with the proportions, usinf function DATA.FRAME
+#crate the array that will compose the data.frame
+landcover <- c("Forest", "Human")
+percent_1992 <- c(89.77, 10.23)
+percent_2006 <- c(51.99, 48.01)
 
+perc <- data.frame(landcover, percent_1992, percent_2006)
+
+#CREATE HISTOGRAMS WITH GGPLOT
+library(ggplot2)
+ggplot(perc, aes(landcover, percent_1992, color=landcover)) + geom_bar(stat="identity", fill = "white")
+#aes = aesthetic = x, y, color
+ggplot(perc, aes(landcover, percent_2006, color=landcover)) + geom_bar(stat="identity", fill = "white")
+
+#if we want a multiframe we can use use par(mfrow()), but easier to use PATCHWORK
+library(patchwork) #adding ggplot elements to objects that we can sum together
+p1 <- ggplot(perc, aes(landcover, percent_1992, color=landcover)) + geom_bar(stat="identity", fill = "white")
+p2 <- ggplot(perc, aes(landcover, percent_2006, color=landcover)) + geom_bar(stat="identity", fill = "white")
+#final plot
+p1 + p2
+
+#plot one on top of the other = ratio
+p1/p2
+
+#RGB ggplot image
+ggRGB(l1992, 1, 2, 3) #the stretch is linear by default
+ggRGB(l2006, 1, 2, 3)
+
+#calculate dvi index
+dvi1992 <- l1992[[1]] - l1992[[2]]
+dvi2006 <- l2006[[1]] - l2006[[2]]
+ggplot() + geom_raster(dvi1992, mapping = aes(x=x, y=y, fill=layer)) #layer è la variabile delle immagini
+
+#fot better and more inderstandable colors
+library(viridis)
+p3 <- ggplot() + geom_raster(dvi1992, mapping = aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="viridis")
+p4 <- ggplot() + geom_raster(dvi2006, mapping = aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="viridis")
+p3 + p4
+#put two graph one by side, with two different viridis colo maps
