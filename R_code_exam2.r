9cfa81f94840019a31db32c3039ac22006be1d8a
Serena <115725865+serenaneg@users.noreply.github.com>
2023-01-21 20:29:05 +0100

    Update R_code_exam2.r

--- R_code_exam2.r
+++ R_code_exam2.r
@@ -9,17 +9,16 @@ library(RColorBrewer)
 setwd("/home/serena/Scrivania/Magistrale/monitoring_ecosystem/alga_bloom/LT05_L1TP_019031_20111005_20200820_02_T1")
 
 #load layers that create the image
-blue <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B1.TIF") #0.45-0.52 nm
-green <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B2.TIF") #0.52-0.60 nm
-red <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B3.TIF") #0.63-0.69 nm
-nir <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B4.TIF") #0.76-0.90 nm
-swir <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B5.TIF") #1.55-1.75 nm
+blue <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B1.TIF") #0.45-0.52 mum
+green <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B2.TIF") #0.52-0.60 mum
+red <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B3.TIF") #0.63-0.69 mum
+nir <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B4.TIF") #0.76-0.90 mum
+swir <- raster("LT05_L1TP_019031_20111005_20200820_02_T1_B5.TIF") #1.55-1.75 mum
 
 file <- stack(blue, green, red, nir, swir)
 
 #cut the image to zoom on the region of interest
-#extention of the original image:
-#extent     : 283185, 430005, 4514385, 4732515  (xmin, xmax, ymin, ymax)
+#extention of the original image: 283185, 430005, 4514385, 4732515  
 boundary <- raster( xmn = 365000, xmx = 430000 , ymn = 4600000, ymx = 4680000)
 image <- crop(file, boundary)
 
@@ -33,13 +32,14 @@ dev.off()
 
 #infrared image
 #create the image with nir band (5) as red, red band (4) green and green band (3) as blue 
-rgb_lw <- stack(swir, nir, red)
+rgb_lw <- stack(red, nir, swir)
 boundary <- raster( xmn = 365000, xmx = 430000 , ymn = 4600000, ymx = 4680000)
 image_lw <- crop(rgb_lw, boundary)
 
 #plot layers red bands
-pdf("rgbFalse.pdf")
-plot(image_lw, xaxt='n', yaxt='n', main = c("SWIR - Band 5", "NIR - Band 4", "Red - Band 3" ))
+pdf("rgbFalse.pdf", width = 7, height = 6)
+par(pty = "m", mar = c(2, 4, 4, 4))
+plot(image_lw, xaxt='n', yaxt='n', main = c( "Red - Band 3", "NIR - Band 4", "SWIR - Band 5"))
 dev.off()
 
 #looking at the swir image band, id the one with grater contrast between land and sea pixels => used to create the mask
@@ -50,13 +50,14 @@ swir_image[swir_image > 20] <- NA
 #apply the mask
 image_masked <- mask(image, mask = swir_image)
 #plot images
-pdf("masked.pdf")
-par(mfrow=c(1,2))
+pdf("masked_rgb.pdf")
 #TRUE COLOR
 plotRGB(image_masked, r = 3, g = 2, b = 1, stretch = "lin")
 legend("top", legend = NA, title = expression(bold("RGB True Color")), bty = "n", cex = 1.3)
+dev.off()
 
 #FALSE COLOR
+pdf("masked_false.pdf")
 plotRGB(image_masked, r = 4, g = 3, b = 2, stretch = "lin") 
 legend("top", legend = NA, title = expression(bold("RGB False Color")), bty = "n", cex = 1.3)
 dev.off()
